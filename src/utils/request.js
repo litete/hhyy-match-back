0af2d31ebe4ed5745dde5bb3bc2cc90e4f0c8f9b
litete <1153681927@qq.com>
2019-08-31 18:39:44 +0800

    fix

---
+++ src/utils/request.js
@@ -0,0 +1,113 @@
+import axios from 'axios'
+import { MessageBox, Message } from 'element-ui'
+import store from '@/store'
+import { getToken } from '@/utils/auth'
+import router from '@/router/index'
+
+// create an axios instance
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // withCredentials: true, // send cookies when cross-domain requests
+  timeout: 5000 // request timeout
+})
+
+// request interceptor
+service.interceptors.request.use(
+  config => {
+    // do something before request is sent
+
+    if (store.getters.token) {
+      // let each request carry token
+      // ['X-Token'] is a custom headers key
+      // please modify it according to the actual situation
+      config.headers['Authorization'] = 'Bearer '+getToken()
+    }
+    return config
+  },
+  error => {
+    // do something with request error
+    console.log(error) // for debug
+    return Promise.reject(error)
+  }
+)
+
+// response interceptor
+service.interceptors.response.use(
+  /**
+   * If you want to get http information such as headers or status
+   * Please return  response => response
+  */
+
+  /**
+   * Determine the request status by custom code
+   * Here is just an example
+   * You can also judge the status by HTTP Status Code
+   */
+  response => {
+    const res = response.data
+
+    // if the custom code is not 20000, it is judged as an error.
+    if (res.code !== 200) {
+      Message({
+        message: res.message || 'Error',
+        type: 'error',
+        duration: 5 * 1000
+      })
+
+      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
+      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+        // to re-login
+        MessageBox.confirm('您已退出，可以取消此页面，或者重新登录', '登录错误', {
+          confirmButtonText: '重新登录',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          store.dispatch('user/resetToken').then(() => {
+            location.reload()
+          })
+        })
+      }
+      return Promise.reject(new Error(res.message || '系统错误'))
+    } else {
+      return response.data
+    }
+  },
+  error => {
+    console.log('err' + error) // for debug
+    if (error.response.status === 403) {
+      router.push({
+        path: '/dashboard'
+      })
+    } else if (error.response.status === 401) {
+      // if (error.response.config.url.includes('auth')) {
+      //   Message({
+      //     // message: error.response.data.message,
+      //     message: '系统升级中，请稍后' || 'error',
+      //     type: 'error',
+      //     duration: 5 * 1000
+      //   })
+      //   console.log('error')
+      // } else {
+      Message({
+        // message: error.response.data.message,
+        message: '认证失败' || 'error',
+        type: 'error',
+        duration: 5 * 1000
+      })
+      console.log('用户已被禁用或无权限操作')
+      // location.reload() // 为了重新实例化vue-router对象 避免bug
+      // router.push('/login')
+      // }
+    } else {
+      Message({
+        // message: error.response.data.message,
+        message: error.message || 'error',
+        type: 'error',
+        duration: 5 * 1000
+      })
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default service
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="dashboard-container" >
+    <div class="dashboard-text">
+      匹配开关:&nbsp;&nbsp;
+      <div >
+        <el-row >
+          <el-button type="primary" plain @click="changeOpen">{{ isOpen }}</el-button>
+        </el-row>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Dashboard",
+  data() {
+    return {
+      isOpen: "开启"
+    }
+    changeOpen(){
+
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+    display: flex;
+  }
+}
+</style>
